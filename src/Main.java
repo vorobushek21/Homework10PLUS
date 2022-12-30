3f966712f32fbccab142bb0409d6f8b9e6c07894
vorobushek <anton.vorobev1999@mail.ru>
2022-12-31 01:15:38 +0300

    Решил 3 задачу

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -16,12 +17,11 @@ public class Main {
         int middleNameFind = fullName.indexOf('I', firstNameFind + 1);
         int middleNameFind2 = fullName.indexOf(' ', firstNameFind2 + 1);
         String middleName = fullName.substring(middleNameFind, middleNameFind2);
-        int lastNameFind = fullName.indexOf('I', middleNameFind + 1);
-        String lastName = fullName.substring(lastNameFind, fullName.length() - 1);
-        System.out.println(firstName + " " + middleName + " " + (lastName + fullName.charAt(fullName.length() - 1)));
+        String lastName = fullName.substring(middleNameFind2+1);
+        System.out.println(firstName + " " + middleName + " " + lastName);
         System.out.println("Фамилия сотрудника - " + firstName);
         System.out.println("Имя сотрудника - " + middleName);
-        System.out.println("Отчество сотрудника - " + (lastName + fullName.charAt(fullName.length() - 1)));
+        System.out.println("Отчество сотрудника - " + lastName);
         System.out.println("Второй вариант решения задачи");
         int[] index = new int[3];
         for (int i = 0, start = 0; i < index.length; i++, start++) {
@@ -35,25 +35,36 @@ public class Main {
         }
         System.out.println("Фамилия сотрудника - " + fullName.substring(index[0], index2[0]));
         System.out.println("Имя сотрудника - " + fullName.substring(index[1], index2[1]));
-        System.out.println("Отчество сотрудника - " + fullName.substring(index[2], fullName.length() - 1) + fullName.charAt(fullName.length() - 1));
+        System.out.println("Отчество сотрудника - " + fullName.substring(index[2]));
+        System.out.println();
     }
+
+    public static String firstLetterToUpperCase(String word) {
+        String i = word.substring(0,1);
+        String r = i.toUpperCase() + word.substring(1);
+        return r;
+    }
+
 
     public static void task2() {
         System.out.println("Задача 2");
         String fullName = "ivanov ivan ivanovich";
         String[] splitFullName = fullName.split(" ");
-        for (int i = 0; i < splitFullName.length; i++) {
-            char[] letter = splitFullName[i].toCharArray();
-            char firstLetter = Character.toUpperCase(letter[0]);
-            letter[0] = firstLetter;
-            for (int j = 0; j < letter.length; j++) {
-                System.out.print(letter[j]);
-
-            }
-            //splitFullName[i] = splitFullName[i].replace(splitFullName[i].charAt(0), firstLetter);
-
-        }
+        System.out.println(firstLetterToUpperCase(splitFullName[0])+" "+firstLetterToUpperCase(splitFullName[1])+" "+firstLetterToUpperCase(splitFullName[2]));
         System.out.println();
-        System.out.println(Arrays.toString(splitFullName));
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        String firstString = "135";
+        String secondString = "246";
+        String[] numbers1 = firstString.split("");
+        String[] numbers2 = secondString.split("");
+        String finalString = "";
+        for (int i = 0; i < numbers1.length; i++) {
+            String lastString = numbers1[i] + numbers2[i];
+            finalString = finalString + lastString;
+        }
+            System.out.println(finalString);
     }
 }
